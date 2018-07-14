b9da2f9785732efb9ab755369d7e00bb7ee758da
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-14 16:36:05 +0300

    Update TagsTemplate.php

--- components/templates for widgets/TagsTemplate.php
+++ components/templates for widgets/TagsTemplate.php
@@ -1,12 +1,12 @@
 <? if ( !empty($tag_list) ) : ?>
-    <div class="product-listy">
-        <div class="tags">
-            <h4 class="tag_head">Tags Widget</h4>
-            <ul class="tags_links">
-                <? foreach($tag_list as $tag) : ?>
-                <li><a href="<?= Yii::$app->urlManager->createUrl(['shop/products', 'tag' => $tag]) ?>"><?= $tag ?></a></li>
-                <? endforeach; ?>
-            </ul>
-        </div>
+  <div class="product-listy">
+    <div class="tags">
+      <h4 class="tag_head">Tags Widget</h4>
+      <ul class="tags_links">
+        <? foreach($tag_list as $tag) : ?>
+        <li><a href="<?= Yii::$app->urlManager->createUrl(['shop/products', 'tag' => $tag]) ?>"><?= $tag ?></a></li>
+        <? endforeach; ?>
+      </ul>
     </div>
+  </div>
 <? endif; ?>
