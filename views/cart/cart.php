1ef3ee43b5313cac4371f3696acc284e7f3f96db
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-14 15:31:45 +0300

    Update cart.php

--- views/cart/cart.php
+++ views/cart/cart.php
@@ -3,60 +3,52 @@
     use yii\widgets\ActiveForm;
 ?>
 
-
 <div class="cart-items" id="ajax-update-cart">
     <div class="container">
-        <div class="dreamcrub">
-            <ul class="breadcrumbs">
-                <li class="home">
-                    <a href="<?=Yii::$app->urlManager->createUrl('site/index')?>" title="Go to Home Page">Home</a>&nbsp;
-                    <span>&gt;</span>
-                </li>
-                <li class="women">
-                    Cart
-                </li>
-            </ul>
-            <div class="clearfix"></div>
-        </div>
-        <h2>MY SHOPPING BAG (<?= count( $_SESSION['cart'] ) ?>)</h2>
-
+        <h2>MY SHOPPING BAG (<span class="count-of-cart"><?= $_SESSION['cart.qty'] ?></span>)</h2>
         <div class="cart-gd">
             <? if ( !empty($products) ) : ?>
+            <div class="col-md-12" style="margin-left: 0px;">
                 <? foreach ( $products as $id => $product ) : ?>
-                <div class="cart-header">
-                    <div class="close1 remove-position" id="product-<?= $id ?>"> </div>
-                    <div class="cart-sec simpleCart_shelfItem">
-                        <div class="test">
-                            <img src="/web/images/product/<?= $product['img'] ?>" class="cart-image" alt="">
+                <div class="col-md-6 item-cart-block">
+                  <div class="position-block">
+                    <div class="close1 remove-position remove" id="product-<?= $id ?>"> </div>
+                    <div>
+                        <div class="col-md-3 col-sm-3 col-xs-4 left-img">
+                            <img src="/web/images/product/<?= $product['img'] ?>" class="cart-image"><!--  -->
                         </div>
 
-                        <div class="cart-item-info">
-                            <div class="name-product-caption">
-                                <h3><a href="#"><?= $product['name_product'] ?></a><span>Pickup time:</span></h3>
+                        <div class="col-md-9 col-sm-9 col-xs-8 description-pos">
+                          <div class="normallize">
+                            <div style="padding-left: 2px;">
+                                <h3><a class="name-product-in-cart" href="#"><?= $product['name_product'] ?></a></h3>
                             </div>
-                            <ul class="qty">
+                            <ul class="list">
                                 <li><p>Min. order value: <input class="cart-qty-input" product="<?= $product['id_product'] ?>" value="<?= $product['qty'] ?>"></p></li>
                                 <li>
-                                    <p>Sizes of product:
-                                        <select class="test-test" id="<?= $id_product ?>">
-                                            <? foreach( $product['sizes'] as $size ) : ?>
-                                            <option value="<?= $size['size'] ?>"><?= $size['size'] ?></option>
-                                            <? endforeach; ?>
-                                        </select>
-                                    </p>
+                                    <div class="inline-size-block">
+                                        <div>Sizes of product: </div>
+                                        <div style="width: inherit;">
+                                            <select class="test-test select-size" id="<?= $id_product ?>">
+                                                <? foreach( $product['sizes'] as $size ) : ?>
+                                                <option value="<?= $size['size'] ?>"><?= $size['size'] ?></option>
+                                                <? endforeach; ?>
+                                            </select>                                            
+                                        </div>
+                                    </div>
                                 </li>
+                                <li class="price-indentation">Price : $<?= sprintf("%.2f", $product['price']/100); ?></li>
                             </ul>
-                            <div class="">
-                                <div class="cart-price">Price : $<?= sprintf("%.2f", $product['price']/100); ?></div>
-                                <!--<span>Delivered in 1-1:30 hours</span>-->
-                            </div>
+                          </div>
                         </div>
+
                         <div class="clearfix"></div>
                     </div>
+                  </div>
                 </div>
                 <? endforeach; ?>
+            </div>
             <? endif; ?>
-
             <div class="confirm-order-btn">
                 <a href="" class="btn" id="open-modal-order-page">Confirm Order</a>
             </div>
@@ -65,8 +57,7 @@
 </div>
 
 
-
-<div id="modal-confirm-order">
+<div id="modal-confirm-order" class="order-modal">
     <? if ( !Yii::$app->user->isGuest ) : ?>
 
         <div id="modal-order-user">
@@ -78,7 +69,7 @@
                 <div class="col-md-12" style="margin-top: 15px;"><?= $f->field($confirm_order_form, 'message')->textArea(['rows' => 4])->label(false) ?></div>
                 <div class="col-md-12">
                     <div class="parent-flex">
-                        <button class="button-confirm-order btn btn-sm" type="submit">Confirm Order</button>
+                        <button class="button-confirm-order btn btn-sm" type="submit" style="margin-left: 5px;">Confirm Order</button>
                     </div>
                 </div>
             </div>
@@ -95,8 +86,8 @@
             <div class="row">
                 <div class="col-md-6"><?= $f->field($confirm_order_form, 'first_name') ?></div>
                 <div class="col-md-6"><?= $f->field($confirm_order_form, 'last_name') ?></div>
-                <div class="col-md-6"><?= $f->field($confirm_order_form, 'mobile_phone') ?></div>
-                <div class="col-md-6"><?= $f->field($confirm_order_form, 'email') ?></div>
+                <div class="col-md-6"><?= $f->field($confirm_order_form, 'mobile_phone')->label('Mobile Phone*') ?></div>
+                <div class="col-md-6"><?= $f->field($confirm_order_form, 'email')->label('Email*') ?></div>
                 <div class="col-md-12"><?= $f->field($confirm_order_form, 'message')->textArea(['row' => 4]) ?></div>
                 <div class="col-md-12">
                     <div class="parent-flex">
@@ -110,3 +101,123 @@
     <? endif; ?>
 </div>
 <div id="overlay"></div>
+
+
+<script>
+
+    $(document).ready(function() {
+        if ( document.documentElement.clientWidth <= 380 ) {
+            $('.left-img').removeClass('col-xs-4');
+            $('.description-pos').removeClass('col-xs-8');
+        } else {
+            if ( !$('left-img').hasClass('col-xs-4') ) {
+                 $('.left-img').addClass('col-xs-4');
+            }
+            if ( !$('.description-pos').hasClass('col-xs-8') ) {
+                 $('.description-pos').addClass('col-xs-8');
+            }
+        }
+    });
+    
+    $(window).resize(function() {
+        if ( document.documentElement.clientWidth <= 380 ) {
+            $('.left-img').removeClass('col-xs-4');
+            $('.description-pos').removeClass('col-xs-8');
+        } else {
+            if ( !$('left-img').hasClass('col-xs-4') ) {
+                 $('.left-img').addClass('col-xs-4');
+            }
+            if ( !$('.description-pos').hasClass('col-xs-8') ) {
+                 $('.description-pos').addClass('col-xs-8');
+            }
+        }
+    });
+
+</script>
+
+
+
+<? if ( Yii::$app->user->isGuest ) : ?>
+<script>
+
+    //styles of confirm for guest
+    $(document).ready(function() {
+        if ( document.documentElement.clientWidth <= 992 ) {
+            $('#modal-confirm-order').css({
+                'overflow-y': 'scroll',
+                'overflow-x': 'hidden',
+                'height': '60%',
+                'margin-top': '-30vh',
+                'width': '90%',
+                'left': '5%'
+            });
+        } else {
+            $('#modal-confirm-order').css({
+                'overflow-y': 'hidden',
+                'overflow-x': 'hidden',
+                'height': 'inherit',
+                'width': '50%',
+                'left': '25%'
+            });
+        }
+    });
+
+    $(window).resize(function() {
+        if ( document.documentElement.clientWidth <= 992 ) {
+            $('#modal-confirm-order').css({
+                'overflow-y': 'scroll',
+                'overflow-x': 'hidden',
+                'height': '60%',
+                'margin-top': '-30vh',
+                'width': '90%',
+                'left': '5%'
+            });
+        } else {
+            $('#modal-confirm-order').css({
+                'overflow-y': 'hidden',
+                'overflow-x': 'hidden',
+                'height': 'inherit',
+                'width': '50%',
+                'left': '25%'
+            });
+        }
+    });
+
+</script>
+<? endif; ?>
+
+
+<? if ( !Yii::$app->user->isGuest ) : ?>
+<script>
+
+    //styles of confirm for user
+    $(document).ready(function() {
+        if ( document.documentElement.clientWidth <= 992 ) {
+            $('#modal-confirm-order').css({
+                'width': '90%',
+                'left': '5%'
+            });
+        } else {
+            $('#modal-confirm-order').css({
+                'width': '50%',
+                'left': '25%'
+            });
+        }
+    });
+
+    $(window).resize(function() {
+        if ( document.documentElement.clientWidth <= 992 ) {
+            $('#modal-confirm-order').css({
+                'width': '90%',
+                'left': '5%'
+            });
+        } else {
+            $('#modal-confirm-order').css({
+                'width': '50%',
+                'left': '25%'
+            });
+        }
+    });
+
+</script>
+<? endif; ?>
