7c1f9b27e48042cb3a717a5f5503fa745b603895
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-14 15:20:32 +0300

    Add files via upload

---
+++ web/js/modalForgetEmail.js
@@ -0,0 +1,21 @@
+function modalForgetEmail() {
+    $('#passwprd-forgot').click(function(event) {
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function(){
+                $('#modal-forgot-password')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '30%'}, 200);
+            });
+    });
+
+    $('#overlay').click( function(){
+        $('#modal-forgot-password')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function(){
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
+        });
+}modalForgetEmail();
