c47403be4dd486f1f593ffbd64a884b7bb9fd17b
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-14 15:19:14 +0300

    Update login.php

--- views/site/login.php
+++ views/site/login.php
@@ -7,25 +7,23 @@
     <div class="container">
         <div class="login-page">
             <div class="account_grid">
-                <div class="col-md-6 login-left wow fadeInLeft" data-wow-delay="0.4s">
+                <div class="col-md-6 col-sm-12 col-xs12 " style="margin-top: 20px;" data-wow-delay="0.4s"><!-- login-left wow fadeInLeft -->
                     <h2>NEW CUSTOMERS</h2>
-                    <p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
-                    <a class="acount-btn" href="<?=Yii::$app->urlManager->createUrl('site/registration')?>">Create an Account</a>
+                    <p style="color: #CAC9C9; margin-bottom: 20px;">By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+                    <a class="acount-btn" style="margin-bottom: 30px;" href="<?=Yii::$app->urlManager->createUrl('site/registration')?>">Create an Account</a>
                 </div>
-                <div class="col-md-6 login-right wow fadeInRight" data-wow-delay="0.4s">
+                <div class="col-md-6 col-sm-12 col-xs12 reg" data-wow-delay="0.4s"><!-- login-right -->
                     <h3>REGISTERED CUSTOMERS</h3>
                     <p>If you have an account with us, please log in.</p>
 
-                    <? $f = ActiveForm::begin(['errorCssClass' => '']); ?>
-                        <div>
+                    <? $login_form = ActiveForm::begin(['errorCssClass' => '']); ?>
                             <span>Email Address<label>*</label></span>
-                            <?=$f->field($login_model, 'email')->label(false)->textInput(['class' => '']);?>
-                        </div>
-                        <div>
+                            <?= $login_form->field($login_model, 'email')->label(false)->textInput(['class' => '']); ?>
+
                             <span>Password<label>*</label></span>
-                            <?=$f->field($login_model, 'password')->label(false)->passwordInput(['class' => '']);?>
-                        </div>
-                        <a class="forgot" href="#">Forgot Your Password?</a>
+                            <?= $login_form->field($login_model, 'password')->label(false)->passwordInput(['class' => '']); ?>
+
+                        <a class="forgot" id="passwprd-forgot" href="<?= Yii::$app->urlManager->createUrl('site/forget-password') ?>">Forgot Your Password?</a>
                         <input type="submit" value="Login">
                     <? ActiveForm::end(); ?>
 
@@ -35,3 +33,19 @@
         </div>
     </div>
 </div>
+
+<!-- модальное окно -->
+<div id="modal-forgot-password">
+    <div><!-- id="modal-order-user" -->
+        <? $email_form = ActiveForm::begin([
+            'enableClientValidation' => false,
+            'enableAjaxValidation' => true,
+            'validationUrl' => '/site/login',
+            'action' => Yii::$app->urlManager->createUrl('site/forget-password')
+        ]); ?>
+        <?= $email_form->field($forget_password_model, 'email') ?>
+        <div class="flex"><input class="submit-button" type="submit" value="Get Password"></div>
+        <? ActiveForm::end(); ?>
+    </div>
+</div>
+<div id="overlay"></div>
