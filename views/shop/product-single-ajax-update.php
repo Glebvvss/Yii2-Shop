99eb083a194a7b76333a9e7a55e758af4fcb4726
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-14 15:57:47 +0300

    Update product-single-ajax-update.php

--- views/shop/product-single-ajax-update.php
+++ views/shop/product-single-ajax-update.php
@@ -1,3 +1,3 @@
 <div class="response" id="ajax-update" data-id="<?= $id_product ?>">
-    <?= app\components\ReviewWidget::widget(['id_product' => $id_product, 'reviews' => $reviews]); ?>
+  <?= app\components\ReviewWidget::widget(['id_product' => $id_product, 'reviews' => $reviews]); ?>
 </div>
