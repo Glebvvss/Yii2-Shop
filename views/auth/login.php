3d39221915b8e1cae643027f184e3d3d49740809
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-29 21:11:29 +0300

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -10,7 +10,7 @@
         <div class="col-md-6 col-sm-12 col-xs12 " style="margin-top: 20px;" data-wow-delay="0.4s"><!-- login-left wow fadeInLeft -->
           <h2>NEW CUSTOMERS</h2>
           <p style="color: #CAC9C9; margin-bottom: 20px;">By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
-          <a class="acount-btn" style="margin-bottom: 30px;" href="<?=Yii::$app->urlManager->createUrl('site/registration')?>">Create an Account</a>
+          <a class="acount-btn" style="margin-bottom: 30px;" href="<?=Yii::$app->urlManager->createUrl('auth/registration')?>">Create an Account</a>
         </div>
         <div class="col-md-6 col-sm-12 col-xs12 reg" data-wow-delay="0.4s"><!-- login-right -->
           <h3>REGISTERED CUSTOMERS</h3>
@@ -21,7 +21,7 @@
             <?= $login_form->field($login_model, 'email')->label(false)->textInput(['class' => '']); ?>
             <span>Password<label>*</label></span>
             <?= $login_form->field($login_model, 'password')->label(false)->passwordInput(['class' => '']); ?>
-            <a class="forgot" id="passwprd-forgot" href="<?= Yii::$app->urlManager->createUrl('site/forget-password') ?>">Forgot Your Password?</a>
+            <a class="forgot" id="passwprd-forgot" href="<?= Yii::$app->urlManager->createUrl('auth/forget-password') ?>">Forgot Your Password?</a>
             <input type="submit" value="Login">
           <? ActiveForm::end(); ?>
 
@@ -38,8 +38,8 @@
     <? $email_form = ActiveForm::begin([
       'enableClientValidation' => false,
       'enableAjaxValidation' => true,
-      'validationUrl' => '/site/forget-password',
-      'action' => Yii::$app->urlManager->createUrl('site/forget-password')
+      'validationUrl' => '/auth/forget-password',
+      'action' => Yii::$app->urlManager->createUrl('auth/forget-password')
     ]); ?>
     <?= $email_form->field($forget_password_model, 'email') ?>
     <div class="flex"><input class="submit-button" type="submit" value="Get Password"></div>
