b595bde08ddc532f70e54dd6b9d2c4e43190695b
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-29 21:15:21 +0300

    Update ShopController.php

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -86,47 +86,4 @@ class ShopController extends Controller {
             'product' => $product,
         ]);
     }
-
-    public function actionAddReviewAjax() {
-        if ( !Yii::$app->request->isAjax ) {
-            $this->redirect('');
-        }
-
-        $id_product = Yii::$app->request->post('id_product');
-        $id_parent_review = Yii::$app->request->post('id_parent_review');
-        $review = Yii::$app->request->post('review');
-        $this->layout = false;
-
-        $builder = new ReviewOperationsBuilder();
-        $reviewOperations = $builder->idParent($id_parent_review)
-                                    ->idProduct($id_product)
-                                    ->review($review)
-                                    ->build();
-
-        $reviewOperations->addReview();
-        $reviews = $reviewOperations->selectReviews();
-        return $this->render('reviews-component-ajax', [
-            'id_product' => $id_product,
-            'reviews' => $reviews
-        ]);
-    }
-
-    public function actionDeleteReviewAjax() {
-        $id_product = Yii::$app->request->post('id_product');
-        $id_review = Yii::$app->request->post('id_review');
-        $this->layout = false;
-
-        $builder = new ReviewOperationsBuilder();
-        $reviewOperations = $builder->id($id_review)
-                                    ->idProduct($id_product)
-                                    ->build();
-
-        $reviewOperations->deleteReview();
-        $reviews = $reviewOperations->selectReviews();
-        return $this->render('reviews-component-ajax', [
-            'id_product' => $id_product,
-            'reviews' => $reviews
-        ]);
-    }
-
 }
