d26be0560219f654958ec51576d0e0df574ee333
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-07-30 03:14:07 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'defaultRoute' => 'shop/index',
+    'defaultRoute' => 'main/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
